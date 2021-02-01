127fa3529eaffd5c43fd5d6f98adead02f502393
alexeyvkuznetsov <57808789+alexeyvkuznetsov@users.noreply.github.com>
2021-02-01 15:28:53 +0300

    Update Isidorus Hispalensis Topic Modeling 2.R

--- Isidorus Hispalensis Topic Modeling 2.R
+++ Isidorus Hispalensis Topic Modeling 2.R
@@ -131,6 +131,59 @@ abline(v=seq(0,85,10), lty=3, col="gray")
 
 
 
+#############################################################
+
+# https://github.com/dateam-jds/textmodelling/blob/main/R/topic%20modeling.R
+
+
+set.seed(12345)
+
+
+k_list <- seq(1, 25, by = 1)
+model_dir <- paste0("models_", digest::digest(colnames(nih_sample_dtm), algo = "sha1"))
+
+# Fit a bunch of LDA models
+model_list <- TmParallelApply(X = k_list, FUN = function(k){
+  
+  m <- FitLdaModel(dtm = dtm, 
+                   k = k, 
+                   iterations = 200, 
+                   burnin = 180,
+                   alpha = 0.1,
+                   beta = colSums(dtm) / sum(dtm) * 100,
+                   optimize_alpha = TRUE,
+                   calc_likelihood = FALSE,
+                   calc_coherence = TRUE,
+                   calc_r2 = FALSE,
+                   cpus = 1)
+  m$k <- k
+  
+  m
+}, export= ls(), # c("nih_sample_dtm"), # export only needed for Windows machines
+cpus = 2) 
+
+#model tuning
+#choosing the best model
+coherence_mat <- data.frame(k = sapply(model_list, function(x) nrow(x$phi)), 
+                            coherence = sapply(model_list, function(x) mean(x$coherence)), 
+                            stringsAsFactors = FALSE)
+
+
+ggplot(coherence_mat, aes(x = k, y = coherence)) +
+  geom_point() +
+  geom_line(group = 1)+
+  ggtitle("Best Topic by Coherence Score") + 
+  theme_minimal() +
+  scale_x_continuous(breaks = seq(1,20,1)) + 
+  ylab("Coherence")
+
+###best model
+model <- model_list[which.max(coherence_mat$coherence)][[1]]
+model$summary <- SummarizeTopics(model)
+
+topic_label <- model$summary %>% select(topic, label_1)
+
+top20_terms <- as.data.frame(model$top_terms)
 
 
 
